44c71d275f69b4416921d4d9d51e68a98a7f4ccd
Ajay Bairwa <bairwaajay770@gmail.com>
2021-01-16 10:52:36 +0530

    create a destination bucket list using gatsby, Leaflet

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,13 @@ module.exports = {
         icon: 'src/assets/images/react-leaflet-icon.png',
       },
     },
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'GCMS',
+        fieldName: 'gcms',
+        url: 'https://api-us-east-1.graphcms.com/v2/ckjyc6ngeojsg01xqh5837way/master',
+      },
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,90 +1,39 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
-import L from 'leaflet';
-import { Marker, useMap } from 'react-leaflet';
+import { Marker, Popup } from 'react-leaflet';
 
-import { promiseToFlyTo, getCurrentLocation } from 'lib/map';
+import { useDestinations } from 'hooks';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
 import Snippet from 'components/Snippet';
 
-import gatsby_astronaut from 'assets/images/gatsby-astronaut.jpg';
-
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
-
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
-
-/**
- * MapEffect
- * @description This is an example of creating an effect used to zoom in and set a popup on load
- */
-
-const MapEffect = ({ markerRef }) => {
-  const map = useMap();
-
-  useEffect(() => {
-    if ( !markerRef.current || !map ) return;
-
-    ( async function run() {
-      const popup = L.popup({
-        maxWidth: 800,
-      });
-
-      const location = await getCurrentLocation().catch(() => LOCATION );
-
-      const { current: marker } = markerRef || {};
-
-      marker.setLatLng( location );
-      popup.setLatLng( location );
-      popup.setContent( popupContentHello );
-
-      setTimeout( async () => {
-        await promiseToFlyTo( map, {
-          zoom: ZOOM,
-          center: location,
-        });
-
-        marker.bindPopup( popup );
-
-        setTimeout(() => marker.openPopup(), timeToOpenPopupAfterZoom );
-        setTimeout(() => marker.setPopupContent( popupContentGatsby ), timeToUpdatePopupAfterZoom );
-      }, timeToZoom );
-    })();
-  }, [map, markerRef]);
-
-  return null;
-};
 
 const IndexPage = () => {
-  const markerRef = useRef();
+  const { destinations } = useDestinations();
+
+  /**
+   * mapEffect
+   * @description Fires a callback once the page renders
+   * @example Here this is and example of being used to zoom in and set a popup on load
+   */
+
+  async function mapEffect({ leafletElement: map } = {}) {
+    if ( !map ) return;
+  }
 
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: 'OpenStreetMap',
     zoom: DEFAULT_ZOOM,
+    mapEffect,
   };
 
   return (
@@ -94,11 +43,25 @@ const IndexPage = () => {
       </Helmet>
 
       <Map {...mapSettings}>
-        <MapEffect markerRef={markerRef} />
-        <Marker ref={markerRef} position={CENTER} />
+        { destinations.map(destination => {
+          const { id, name, location } = destination;
+          const position = [location.latitude, location.longitude];
+          return (
+            <Marker key={id} position={position}>
+              <Popup>{ name }</Popup>
+            </Marker>
+          );
+        })}
       </Map>
 
       <Container type="content" className="text-center home-start">
+        <h2>My Destinations</h2>
+        <ul>
+          { destinations.map(destination => {
+            const { id, name } = destination;
+            return <li key={id}>{ name }</li>
+          })}
+        </ul>
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
         <Snippet>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</Snippet>
